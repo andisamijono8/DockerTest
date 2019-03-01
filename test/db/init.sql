4ff151af04505687a30ef4210c54d5d409630a87
Andi Samijono <andi3173@gmail.com>
2019-03-01 15:34:29 +1100

    added sql db:

---
+++ test/db/init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE students;
+use students;
+
+CREATE TABLE student_dreams (
+  name VARCHAR(20),
+  dream VARCHAR(20)
+);
+
+INSERT INTO student_dreams
+  (name, dream)
+VALUES
+  ('Rahul', 'Husband'),
+  ('Min Xue', 'Wife'),
+  ('Ethan', "Best Programmer");
